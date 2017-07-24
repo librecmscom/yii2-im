a5da7a58ea272f745535bf6aa3bd196f871048c0
xuchao <85825770@qq.com>
2017-07-24 18:30:43 +0800

    update

---
+++ frontend/controllers/ImController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\im\frontend\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Class ImController
+ * @package yuncms\im\frontend\controllers
+ */
+class ImController extends Controller
+{
+
+}
